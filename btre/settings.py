323f7ca112954aed1769ea4c6520456603ecf2ed
Ahmad Ali <sahilahmad2550@gmail.com>
2020-04-21 22:12:45 +0500

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'getraised123@gmail.com'
 EMAIL_HOST_PASSWORD = '00940981@bahl'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
